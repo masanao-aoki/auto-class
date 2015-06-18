63fcacf11bbc37d1804a997ad5d06148fd825d62
aoki <aoki@cubit.jp>
2015-06-18 14:47:18 +0900

    devブランチ

--- root/js/common.js
+++ root/js/common.js
@@ -1,14 +1,32 @@
 (function($) {
-	$.fn.autoClass = function(){
-		
-    var $animeSine = $(this);
+	$.fn.autoClass = function(options){
+
+		c = $.extend({
+			autoClass : {},
+			type : 'normal'
+		},options);
+
+		type = c.type;
+
+
+		var $animeSine = $(this);
 		var animeCount = $animeSine.length;
 		var animeArray = []
 
+		//配列の作成
 		$animeSine.each(function (i) {
 			n = i + 1;
 			animeArray[i] = $('[data-anime-sine='+n+']').data();
 		});
+
+
+		autoClass = {
+			normal : function() {
+				startAnime(0);
+			}
+		};
+
+
 
 		function complateAnime (Num,target) {
 			target.addClass('anime_end');
@@ -20,11 +38,13 @@
 				$('[data-anime-sine='+animeArray[Num].animeSine+']').addClass('anime_start').on('transitionend', function() {
 					complateAnime(Num,$(this));
 				});;
-			} 
+			}
 		}
 
-		startAnime(0);
-		
+
+		autoClass[type]();
+		//startAnime(0);
+
 
     return(this);
   };
